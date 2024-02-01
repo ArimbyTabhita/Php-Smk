d039987567f30a9aa747a4a4bf4473141e3355f1
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-02-01 19:45:39 +0700

    Php Vid 5
    Operator PHP

---
+++ let04.php
@@ -0,0 +1,39 @@
+<?php
+// Operator Matematika
+
+$a=6;
+$b=8;
+$c=$a+$b;
+echo $c.'<br>';
+$c=$a-$b;
+echo $c.'<br>';
+$c=$a*$b;
+echo $c.'<br>';
+$c=$a/$b;
+echo  $c.'<br>';
+$c=$a%$b;
+echo $c;
+
+// Operator Logika
+
+$c=$a<$b;
+echo $c;
+$c=$a>$b;
+echo $c;
+$c=$a==$b;
+echo $c;
+$c=$a!=$b;
+echo $c.'<br>';
+
+// Increment
+
+$a++;
+echo $a.'<br>';
+ // Operator String
+
+$kata='sura';
+$kota='baya';
+$hasil=$kata.$kota;
+$hasil.='kota pahlawan';
+echo $hasil;
+?>

822e3285f70ffd08678afae34217648e15760a87
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-02-01 20:02:09 +0700

    Php Vid 6
    Pengujian IF

---
+++ lat05.php
@@ -0,0 +1,28 @@
+<?php
+$tanggal=0;
+// if ($tanggal<32){
+//     if($tanggal>0){
+//         echo 'benar';
+//     } else {
+//         echo 'salah';
+//     }
+// }  else{
+//     echo 'salah';
+// }
+
+$nilai=4;
+// if($nilai<=100){
+//     if($nilai>=0){
+//     echo 'nilai benar';
+// } else{
+//     echo 'nilai salah';
+//      }
+//     }else{
+//     echo 'nilai salah';
+
+if($nilai>=100 || $nilai<=0){
+    echo 'nilai salah';
+} else {
+    echo 'nilai benar';
+}
+?>

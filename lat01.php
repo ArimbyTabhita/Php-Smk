03da8d80052a532863b41e6064787a6bbf96fde5
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-02-01 19:13:13 +0700

    Php Vid 2
    Menampilkan tulisan di php

---
+++ lat01.php
@@ -0,0 +1,11 @@
+<?php
+    echo"Belajar php";
+    echo '<br>';
+    echo 'Saya siswa smk';
+    echo '<h1 style="background-color: red"> Belajar php itu susah</h1>
+            <p style="background-color: blue">Saya suka belajar pemrograman</p>
+    ';
+    echo 'Saya belajar'.'php'.'<br>';
+    echo 2019 + 2;
+?>
+

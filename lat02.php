ff876e809b25de5c4f44e489e048e57255d8cf56
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-02-01 19:24:22 +0700

    Vid 3
    Variabel,tipe data, dan komentar

---
+++ lat02.php
@@ -0,0 +1,12 @@
+<?php
+    $tulisan = 'Saya Belajar php';
+
+    echo $tulisan. '<br>';
+
+    $angka = 2024;
+    echo 'Tahun '.$angka.'<br>';
+
+    var_dump($tulisan);
+    echo '<br>';
+    var_dump((int)$angka+2);
+?>

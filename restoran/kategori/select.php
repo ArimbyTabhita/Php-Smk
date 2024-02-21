f132c6b8378c1a1b5529a600288a381b8dc47c54
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-02-21 21:13:30 +0700

    Php Vid 38
    Front End

--- restoran/kategori/select.php
+++ restoran/kategori/select.php
@@ -32,6 +32,7 @@ if (isset($_GET['p'])) {
             </tr>
         </thead>
         <tbody>
+        <?php if(!empty($row)){?>
             <?php foreach($row as $r):?>
             <tr>
                 <td><?php echo $no++?></td>
@@ -40,6 +41,7 @@ if (isset($_GET['p'])) {
                 <td><a href="?f=kategori&m=update&id=<?php echo $r['idkategori']?>">Update</a></td>
             </tr>
             <?php endforeach?>
+            <?php }?>
         </tbody>
     </table>
 

140e1d96bfd9b6e57a7e59ebb7f7243ffc997f41
ArimbyTabhita <140997448+ArimbyTabhita@users.noreply.github.com>
2024-02-22 12:02:24 +0700

    Php Vid 44(typo)

--- restoran/home/info.php
+++ restoran/home/info.php
@@ -1,4 +1,4 @@
 <h2>Registrasi Berhasil Silahkan Login</h2>
 <div class="float-left mr-4">
-    <a class="btn btn-primary" href="f=home&=logi" role="button">Login</a>
+    <a class="btn btn-primary" href="f=home&=login" role="button">Login</a>
 </div>
